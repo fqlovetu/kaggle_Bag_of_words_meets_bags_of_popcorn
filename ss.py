e77fadc7da568aaa92ed8daaa1158d2bccc05b78
fqlovetu <59127466+fqlovetu@users.noreply.github.com>
2020-07-08 16:44:45 +0800

    update

---
+++ ss.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8  -*-
+# ！/usr/bin/env python
+# @Time :2020/7/8 16:44 
+# @Author :Sheng Chen
+# @Email :sheng.chen@inossem.com
+import sys
+
+sys.path.append('/home/chensheng/kaggle_Bag of words meets bags of popcorn')
+from typing import List
